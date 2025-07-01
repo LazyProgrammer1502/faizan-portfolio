99eecaec32c16c9390c4f78583d8d559e36fe6a1
APPLE <apple@APPLEs-MacBook-Air.local>
2025-07-01 06:33:20 -0700

    Updated favicon icon and title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   const links = [
     { name: "Home", href: "#home" },
     { name: "About", href: "#about" },
-    { name: "Projects", href: "#projects" },
     { name: "Resume", href: "#resume" },
+    { name: "Projects", href: "#projects" },
     { name: "Contact", href: "#contact" },
   ];
 
